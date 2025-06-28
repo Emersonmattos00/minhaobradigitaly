5a0bfa7e9bd0374db012e208615fd07de066c4fd
Emersonmattos00 <emerson.mattos00@gmail.com>
2025-06-28 15:08:07 -0300

    Update firebase.js

--- minha-obra-digital/app/lib/firebase.js
+++ minha-obra-digital/app/lib/firebase.js
@@ -3,9 +3,9 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  apiKey: AIzaSyAN4scwNv27GPFBZuvAXeWpIQv1Up6vA5E,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  projectId: minha-obra-digitaly,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
